daf7c4ce88df5c4dbd501612d717e6a1f7ccb752
ashutoshvyas2 <146797030+ashutoshvyas2@users.noreply.github.com>
2025-11-03 21:17:57 +0530

    modified:   src/components/Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -188,7 +188,7 @@ export default function Navbar() {
         >
           {/* Re-adding the Logo Image Link here */}
           <img 
-            src="/vt_logo.jpg"
+            src={process.env.PUBLIC_URL + "/vt_logo.jpg"}
             alt="" 
             style={{ height: '30px', width: 'auto', display: 'block' }} 
           />
